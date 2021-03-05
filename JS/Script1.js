033c673975cb2a1e595da86eb2325fa474a36ba0
Wasteoidz <71650988+Wasteoidz@users.noreply.github.com>
2021-03-05 23:34:33 +0100

    update script
    fixed issue with unfilled pompt in lovecalc

--- JS/Script1.js
+++ JS/Script1.js
@@ -1,19 +1,32 @@
 function lovecalc() {
     let n = Math.random() * 100;
     n = Math.floor(n) + 1;
-    let a = prompt("what is your name?")
-    let b = prompt("what is your crush's name?")
-    alert(a + " + " + b + " is a " + n + "% match!")
+    let a = prompt("what is your name?");
+    let b = prompt("what is your crush's name?");
+   
 
-    if (n < 20) {
+    if (a.length < 1 || b.length < 1) {
+        let n = null;
+        alert("One or both of the names are missing! please fill them out correctly");
+    } else if (n < 20) {
+        alert(a + " + " + b + " is a " + n + "% match!");
         alert("There is ls little to no love, do not persue this unless you want your heart broke!");
+        
     } else if (n < 40) {
+        alert(a + " + " + b + " is a " + n + "% match!");
         alert("your love is fading, try new things to rekindle the love flame!");
+        
     } else if (n < 60) {
+        alert(a + " + " + b + " is a " + n + "% match!");
         alert("Love is blossoming, make that extra effort to make it blooming!");
+        
     } else if (n < 80) {
+        alert(a + " + " + b + " is a " + n + "% match!");
         alert("Sparks and lightning fly when you connect, this is pure love and passion!");
+        
     } else if (n < 100) {
-        alert("You have found your true soulmate, arrange a marrige because this is the one for you!");
-    }
+        alert(a + " + " + b + " is a " + n + "% match!");
+        alert("You have found your true soulmate, arrange a marrige because this is the one for you!");        
+    } 
+    
 }
